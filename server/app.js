cd2138087142cd33a4883c17dd449a3ab0093780
mnacfil <mnacfil@gmail.com>
2023-04-12 20:47:57 +0800

    setup socket io

--- server/app.js
+++ server/app.js
@@ -8,19 +8,29 @@ const errorHandler = require('./middlewares/error-handler');
 const userRoute = require('./routes/userRoute');
 const messageRoute = require('./routes/messageRoute');
 const conversationRoute = require('./routes/userConversationRoute');
+const io = require('socket.io')( 5500, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+})
 
 app.use(express.json());
 app.use(cors())
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/message', messageRoute);
 app.use('/api/v1/conversation', conversationRoute);
-
-// testing route
-app.get('/test', (req, res) => {
-    res.json({ message: "Hello World"})
-})
-
 app.use(errorHandler);
+
+// socket io
+io.on('connection', (socket) => {
+    console.log(`user just connected with id: ${socket.id}`);
+
+    io.emit('hello', 'hello world')
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    })
+})
 
 const port = process.env.PORT || 6000;
 
